8094c274e891ed33bb1d9560ce8defc42ecfdbac
Gabriel Syriani <gabrielsyriani@hotmail.ca>
2026-01-05 00:27:20 +0000

    intermediate makefile to test

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -7,7 +7,7 @@ mkdir -p results/rtx3080
 make
 
 # List of matrix sizes
-sizes=(256 512 1024 2048 4096 8192 16384)
+sizes=(256 512 1024 2048 4096 8192)
 
 # Number of runs per size
 num_runs=5
@@ -16,7 +16,7 @@ num_runs=5
 output_file="results/rtx3080/sgemm.csv"
 
 # Add header
-echo "N,CuBLAS,UncoalescedNaive,CoalescedNaive,Tiled,RegisterTiled" > "$output_file"
+echo "N,CuBLAS,UncoalescedNaive,CoalescedNaive,Tiled" > "$output_file"
 
 # Loop over matrix sizes
 for N in "${sizes[@]}"; do
